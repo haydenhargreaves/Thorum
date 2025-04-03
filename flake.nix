300927de514509ebec7a13025615a6413160b6ee
Hayden Hargreaves <hhargreaves2006@gmail.com>
2025-04-02 18:43:26 -0700

    WORKFLOW: Trying a different method
    Not using the flake anymore

--- flake.nix
+++ flake.nix
@@ -11,25 +11,6 @@
       };
     in
       {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          zsh
-          git
-          odin
-          ols
-          ncurses
-        ];
-
-
-        # Simply just exec zsh
-        shellHook = ''
-          # git submodule update --init --recursive
-
-          export LD_LIBRARY_PATH=\"${pkgs.lib.makeLibraryPath [ pkgs.ncurses ]}:$LD_LIBRARY_PATH\"
-          exec zsh
-          '';
-
-      };
       devShell.${system} = pkgs.mkShell {
         packages = with pkgs; [
           zsh
